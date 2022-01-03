005ecd8cea4c91319b559470ce600cc59e100653
SUPERKEON\keon <up2sky7@naver.com>
2022-01-03 18:31:34 +0900

    초기 커밋.

---
+++ BOJ/BOJ10250.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+#pragma warning(disable:4996)
+
+int main() {
+    int T = 0;
+    int H = 0, W = 0, N = 0;
+    scanf("%d", &T);
+    for (int i = 0; i < T; i++) {
+        scanf("%d %d %d", &H, &W, &N);
+
+    }
+    printf("%d", fac(4));
+    return 0;
+}
---
+++ BOJ/BOJ11050.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+#pragma warning(disable:4996)
+
+int fac(int n) {
+    int result = 1;
+    for (int i = n; i > 0; i--) {
+        result *= i;
+    }
+    return result;
+}
+
+int main() {
+    int N = 0, K = 0;
+    int result = 0;
+    scanf("%d %d", &N, &K);
+    if (K < 0 || K > N) {
+        result = 0;
+    }
+    else {
+        result = fac(N) / (fac(K) * fac(N - K));
+    }
+    printf("%d", result);
+    return 0;
+}
