e700f098b726646fb0bc5244135c24c3575527f7
SUPERKEON\keon <up2sky7@naver.com>
2022-02-27 23:04:34 +0900

    조합 - 실패.. long long 으로도 부족

---
+++ BOJ_Class4/조합.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+string combination[100][100];
+
+void Comb(int n, int m) {
+	for (int i = 2; i < n; i++) {
+		for (int j = 2; j < m; j++) {
+			combination[][]
+		}
+	}
+}
+
+int main(void) {
+	int n, m;
+	cin >> n >> m;
+	combination[1][1] = '1';
+	combination[2][1] = '2';
+}
