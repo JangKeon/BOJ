500970b7383db00b9e448aba8806995f98e9b177
SUPERKEON\keon <up2sky7@naver.com>
2022-02-28 23:43:18 +0900

    Class 4 Start 조합

--- BOJ_Class4/조합.cpp
+++ BOJ_Class4/조합.cpp
@@ -1,19 +1,44 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-string combination[100][100];
+string dp[101][101];
 
-void Comb(int n, int m) {
-	for (int i = 2; i < n; i++) {
-		for (int j = 2; j < m; j++) {
-			combination[][]
+string sumString(string a, string b) {
+	int sum = 0;
+	string ans = "";
+	
+	while (!a.empty() || !b.empty() || sum != 0) {
+		if (!a.empty()) {
+			sum += a.back() - '0';
+			a.pop_back();
 		}
+
+		if (!b.empty()) {
+			sum += b.back() - '0';
+			b.pop_back();
+		}
+		ans.push_back((sum % 10) + '0');
+		sum = sum / 10;
 	}
+	reverse(ans.begin(), ans.end());
+	return ans;
+}
+
+string Comb(int n, int m) {
+	if (n == m || m == 0) {
+		return "1";
+	}
+	if (dp[n][m] != "") {
+		return dp[n][m];
+	}
+
+	dp[n][m] = sumString(Comb(n - 1, m), Comb(n - 1, m - 1));
+	return dp[n][m];
 }
 
 int main(void) {
 	int n, m;
 	cin >> n >> m;
-	combination[1][1] = '1';
-	combination[2][1] = '2';
+	cout << Comb(n, m);
 }
