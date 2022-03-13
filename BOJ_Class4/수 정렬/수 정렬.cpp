109b735f7e26aa55eb7cfa5521fb7aba17dba3fc
SUPERKEON\keon <up2sky7@naver.com>
2022-03-13 22:23:16 +0900

    수 정렬

--- BOJ_Class4/수 정렬/수 정렬.cpp
+++ BOJ_Class4/수 정렬/수 정렬.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main(void) {
+	int N, num;
+	int arr[10001] = {0, };
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
-	int N, num;
-	int arr[10001] = { 0, };
 	cin >> N;
 	for (int i = 0; i < N; i++) {
 		cin >> num;
