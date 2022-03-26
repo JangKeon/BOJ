b2e353ee1bb9accb558a75fdf7964b1c3b9ed3f6
SUPERKEON\keon <up2sky7@naver.com>
2022-03-26 22:09:01 +0900

    절댓값 힙 (연산자 생성 이 문제 보고 하자..)

---
+++ BOJ_Class3/절댓값 힙.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+#include <stdlib.h>
+using namespace std;
+
+struct compare {
+	bool operator()(int a, int b) {
+		if (abs(a) == abs(b)) {
+			return a > b;
+		}
+		else return abs(a) > abs(b);
+	}
+};
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	
+	priority_queue <int, vector <int>, compare> pq;
+	int N, exc, res;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> exc;
+		if (exc == 0) {
+			if (pq.empty()) {
+				cout << "0\n";
+			}
+			else { 
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+		else{
+			pq.push(exc);
+		}
+	}
+}
+
