e98d667dd1fe9f23a72bc22e30a08e7e43b1adbb
SUPERKEON\keon <up2sky7@naver.com>
2022-03-31 21:40:44 +0900

    트리의 부모 찾기

--- BOJ_Class4/트리의 부모 찾기.cpp
+++ BOJ_Class4/트리의 부모 찾기.cpp
@@ -2,30 +2,28 @@
 #include <vector>
 using namespace std;
 
-int n;
 int parent[100001];
-vector <int> v[100001];
+vector <int> tree[100001];
 
 void dfs(int x) {
-    for (int i = 0; i < v[x].size(); i++) {
-        if (!parent[v[x][i]]) {
-            parent[v[x][i]] = x;
-            dfs(v[x][i]);
-        }
-    }
+	for (int i = 0; i < tree[x].size(); i++) {
+		if (!parent[tree[x][i]]) {
+			parent[tree[x][i]] = x;
+			dfs(tree[x][i]);
+		}
+	}
 }
 
-int main() {
-    cin >> n;
-    for (int i = 0; i < n - 1; i++) {
-        int x, y;
-        cin >> x >> y;
-        v[x].push_back(y);
-        v[y].push_back(x);
-    }
-
-    dfs(1);
-    for (int i = 2; i <= n; i++) {
-        cout << parent[i] << '\n';
-    }
+int main(void) {
+	int N, x, y;
+	cin >> N;
+	for (int i = 0; i < N - 1; i++) {
+		cin >> x >> y;
+		tree[x].push_back(y);
+		tree[y].push_back(x);
+	}
+	dfs(1);
+	for (int i = 2; i <= N; i++) {
+		cout << parent[i] << '\n';
+	}
 }
