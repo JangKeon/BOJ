02a6f41510ad8bd294d1a26557108b25108443b7
SUPERKEON\keon <up2sky7@naver.com>
2022-04-03 22:58:28 +0900

    N-Queens

---
+++ BOJ_Class4/N-Queens.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int n, res = 0;
+int board[15];
+
+void NQueens(int col) {
+	if (col == n) {
+		res++;
+	}
+	for (int i = 0; i < n; i++) {
+		bool check = true;
+		for (int j = 0; j < col; j++) {
+			board[col] = i;
+			if (board[j] == board[col] || j - col == board[j] - board[col] || j - col == board[col] - board[j]) {
+				check = false;
+				break;
+			}
+		}
+		if (check) {
+			NQueens(col+1);
+		}
+	}
+
+}
+
+int main(void) {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		board[0] = i;
+		NQueens(1);
+	}
+	cout << res;
+}
