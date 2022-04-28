57af217b293efbd7f5071b798af7b13fc1b2dc0e
SUPERKEON\keon <up2sky7@naver.com>
2022-04-28 22:55:17 +0900

    피보나치

---
+++ BOJ/피보나치.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int dp[25];
+
+int main(void) {
+	int N;
+	cin >> N;
+
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 1;
+
+	for (int i = 3; i <= 20; i ++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+
+	cout << dp[N];
+}
