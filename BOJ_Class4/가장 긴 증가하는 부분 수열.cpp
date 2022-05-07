f2048f2f1cc432ea2e80b21eca66c1efab6d0fb4
SUPERKEON\keon <up2sky7@naver.com>
2022-05-07 22:36:44 +0900

    가장 긴 증가하는 부분 수열

--- BOJ_Class4/가장 긴 증가하는 부분 수열.cpp
+++ BOJ_Class4/가장 긴 증가하는 부분 수열.cpp
@@ -2,20 +2,24 @@
 #include <algorithm>
 using namespace std;
 
-int arr[1001], dp[1001], ans, N;
 
 int main(void) {
+    int N, res = 0;
+    int arr[1001], dp[1001] = { 0, };
     cin >> N;
-    for (int i = 1; i <= N; ++i) {
+    for (int i = 1; i <= N; i++) {
         cin >> arr[i];
-        int here = 0;
-        for (int j = 1; j < i; ++j) {
-            if (arr[i] > arr[j])
-                here = max(here, dp[j]);
-        }
-        dp[i] = here + 1;
-        ans = max(ans, dp[i]);
     }
-    cout << ans;
+    fill(dp, dp + 1001, 1);
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j < i; j++) {
+            if (arr[i] > arr[j]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        res = max(res, dp[i]);
+    }
+
+    cout << res;
 }
 
