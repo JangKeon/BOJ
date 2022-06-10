c3765747bd7c83bca1152b975cc595926b73b8af
SUPERKEON\keon <up2sky7@naver.com>
2022-06-10 21:01:32 +0900

    이친수

--- BOJ_Class4/이친수.cpp
+++ BOJ_Class4/이친수.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 using namespace std;
 
+long long dp[91][2];
+
 int main(void) {
-	int dp[91] = { 0, 1, 1, 2, 3, 5, };
-	for (int i = 6; i < 91; i++) {
-		dp[i] = dp[i - 1] + dp[i - 2];
-	}
 	int N;
-	cin >> N; 
-	cout << dp[N]
+	cin >> N;
+	dp[1][0] = 0;
+	dp[1][1] = 1;
+	for (int i = 2; i <= N; i++) {
+		dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+		dp[i][1] = dp[i - 1][0];
+	}
+	cout << dp[N][0] + dp[N][1];
 }
