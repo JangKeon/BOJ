de39137b4455b6f8250d614b2c83a81c2826c023
SUPERKEON\keon <up2sky7@naver.com>
2022-06-18 16:33:38 +0900

    5의 수난

---
+++ BOJ/5의 수난.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+int main(void) {
+	int num, result = 0;
+	
+	for (int i = 0; i < 5; i++) {
+		scanf_s("%1d", &num);
+		result += pow(num, 5);
+	}
+
+	cout << result;
+}
