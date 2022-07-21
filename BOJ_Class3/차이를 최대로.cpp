fa647c6fb8ac0bfc7e50a6ed0db4b047555957d3
SUPERKEON\keon <up2sky7@naver.com>
2022-07-21 20:20:57 +0900

    차이를 최대로 - Greedy X

--- BOJ_Class3/차이를 최대로.cpp
+++ BOJ_Class3/차이를 최대로.cpp
@@ -12,16 +12,32 @@ int main(void) {
 		vec.push_back(num);
 	}
 	sort(vec.begin(), vec.end(), greater<>());
-	int result = 0;
-	for (int i = 0; i < N - 1; i++) {
-		if (N % 2 == 0) {
-			for (int i = 0; i < N / 2 - 1; i++) {
-				result += abs(vec[i] - vec[N - i - 1]);
-			}
-			result += abs(vec[0] - vec[N - 1])
-		}
-		else {
-
+	cout << "Vec 1 : \n";
+	for (int i = 0; i < vec.size(); i++) {
+		cout << vec[i] << ' ';
+	}
+	cout << '\n'; 
+	vector <int> vec2;
+	if (N % 2 == 0) {
+		for (int i = 0; i < vec.size() / 2; i++) {
+			vec2.push_back(vec[N - 1 - i]);
+			vec2.push_back(vec[i]);
 		}
 	}
+	else {
+		for (int i = 0; i < vec.size() / 2; i++) {
+			vec2.push_back(vec[N - 1 - i]);
+			vec2.push_back(vec[i]);
+		}
+		vec2.push_back(vec[N / 2]);
+	}
+	cout << "Vec 2 : \n";
+	for (int i = 0; i < vec2.size(); i++) {
+		cout << vec2[i] << ' ';
+	}
+	int result = 0;
+	for (int i = 0; i < N - 1; i++) {
+		result += abs(vec2[i] - vec2[i + 1]);
+	}
+	cout << result;
 }
