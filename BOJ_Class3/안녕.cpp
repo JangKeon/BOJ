dacab2be9492bfc7645adcca541719fe60f7a13a
SUPERKEON\keon <up2sky7@naver.com>
2022-08-04 16:39:54 +0900

    안녕 - DP

---
+++ BOJ_Class3/안녕.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int dp[101];
+
+struct P {
+	int H;
+	int J;
+};
+
+bool compare(P a, P b) {
+	if (a.H == b.H) {
+		return a.J > b.J;
+	}
+	return a.H < b.H;
+}
+
+int main(void) {
+	vector <P> vec;
+	int N, health[20], joy[20];
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> health[i];
+	}
+	for (int i = 0; i < N; i++) {
+		cin >> joy[i];
+	}
+	/*for (int i = 0; i < N; i++) {	 필요 없음
+		vec.push_back({ health[i], joy[i] });
+	}
+	sort(vec.begin(), vec.end(), compare);*/
+	for (int i = 0; i < N; i++) {
+		for (int j = 100; j >= health[i]; j--) {
+			dp[j] = max(dp[j - health[i]] + joy[i], dp[j]);
+		}
+	}
+	int M = 0;
+	for (int i = 1; i < 100; i++) {
+		if (M < dp[i]) M = dp[i];
+	}
+	cout << M;
+}
