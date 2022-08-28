552765faaa8eef96fe87f73e1a14d54b6d892008
SUPERKEON\keon <up2sky7@naver.com>
2022-08-28 21:39:18 +0900

    약수

--- BOJ_Class3/약수.cpp
+++ BOJ_Class3/약수.cpp
@@ -1,38 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <queue>
 using namespace std;
-#pragma warning(disable:4996)
-
-struct compare {
-	bool operator()(string a, string b) {
-		return a > b;
-	}
-};
 
 int main(void) {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-	int N, M;
-	string name;
-	vector <string> Names;
-	vector <string> Result;
-	cin >> N >> M;
-	for (int i = 0; i < N + M; i++) {
-		cin >> name;
-		Names.push_back(name);
+	int N, num;
+	cin >> N;
+	vector <int> vec;
+	for (int i = 0; i < N; i++) {
+		cin >> num;
+		vec.push_back(num);
 	}
-	sort(Names.begin(), Names.end());
-	for (int i = 1; i < Names.size(); i++) {
-		if (Names[i] == Names[i - 1]) {
-			Result.push_back(Names[i]);
-		}
-
-	}
-
-	cout << Result.size() << '\n';
-	for (int i = 0; i < Result.size(); i++) {
-		cout << Result[i] << '\n';
-	}
+	sort(vec.begin(), vec.end());
+	int result = vec[0] * vec[N - 1];
+	cout << result;
 }
