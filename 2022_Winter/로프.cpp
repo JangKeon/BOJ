e8d712d560b5d2bede091a111005aee442206ec3
Jangkeon <up2sky7@naver.com>
2023-01-01 23:19:51 +0900

    로프 - Greedy

--- 2022_Winter/로프.cpp
+++ 2022_Winter/로프.cpp
@@ -6,13 +6,19 @@ using namespace std;
 int main(void) {
 	int N, num;
 	vector <int> vec;
-	int answer = 99999999;
+	int answer;
 	cin >> N;
 	for (int i = 0; i < N; i++) {
 		cin >> num;
 		vec.push_back(num);
 	}
 	sort(vec.begin(), vec.end());
- // 정렬되어 있으니까 두 번째 부터 i 개로 나누었을 때와 최댓값을 비교한 거를 저장해놓는다.
+	answer = vec[N - 1];
+	for (int i = N - 2; i >= 0; i--) {
+		int sum = vec[i] * (N - i); 
+		if (answer < sum) answer = sum;
+	}
+	cout << answer << '\n';
+ // 정렬되어 있으니까 두 번째 부터 i 개를 곱했을 때와 최댓값을 비교한 거를 저장해놓는다.
  // 이후부터는 저장된 전 값이랑만 비교하면 되니까? DP 형식으로 풀어보자.
 }
