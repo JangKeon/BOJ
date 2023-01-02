3702d30b0a1e147adbdf4ef8101b7dc029a17820
Jangkeon <up2sky7@naver.com>
2023-01-02 23:36:25 +0900

    예산

---
+++ 2022_Winter/예산.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	int N, M, num, sum = 0;
+	vector <int> vec;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> num;
+		sum += num;
+		vec.push_back(num);
+	}
+	cin >> M;
+	sort(vec.begin(), vec.end());
+	if (sum <= M) {
+		cout << vec[N - 1] << '\n';
+	}
+	else {
+
+	}
+}
