ed8f1245d01b35ceaa021bac0c77b7e90016c22c
Jangkeon <up2sky7@naver.com>
2023-05-01 23:37:44 +0900

    [자료 구조, 중복 제거] 좌표 찾기

---
+++ 2023/좌표 찾기.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	int N, num;
+	vector <int> origin;
+	vector <int> sorting;
+	map <int, int> m;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> num;
+		origin.push_back(num);
+		sorting.push_back(num);
+	}
+	sort(sorting.begin(), sorting.end());
+	unique(sorting.begin(), sorting.end());
+	for (int i = 0; i < sorting.size(); i++) {
+		m.insert({ sorting[i], i });
+	}
+	for (int i = 0; i < origin.size(); i++) {
+		cout << m[origin[i]] << ' ';
+	}
+}
